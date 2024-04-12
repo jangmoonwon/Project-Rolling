adebdaeb4720cbb044d8a8d58bcf9cb3c8d7ae8d
jangmoonwon <muneon96@naver.com>
2024-04-12 23:41:11 +0900

    Feat: 랜딩페이지 라우터 작업

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { HomePage } from "./HomePage/HomePage";
 import PlayGround from "./Playground";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<PlayGround />}></Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<PlayGround />} />
       </Routes>
     </BrowserRouter>
   );
