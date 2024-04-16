00d31678fb2510fa5395438dafe6dc229fbd0c1e
jangmoonwon <muneon96@naver.com>
2024-04-16 23:01:29 +0900

    Refactor: empty card 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { HomePage } from "./pages/HomePage/HomePage";
 import { MessagePage } from "./pages/MessagePage/MessagePage";
 import { BackgroundPage } from "pages/BackgroundPage/BackgroundPage";
 import PlayGround from "./Playground";
-import { LinkPage } from "pages/LinkPage/LinkPage";
+import { ListPage } from "pages/ListPage/ListPage";
 import { PostPage } from "./pages/PostPage/PostPage";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/list" element={<LinkPage />} />
+        <Route path="/list" element={<ListPage />} />
         <Route path="/post" element={<BackgroundPage />} />
         <Route path="/post/5864" element={<PostPage />} />
         <Route path="/post/{id}/message" element={<MessagePage />} />
