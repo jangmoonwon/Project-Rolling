02832fd00726be6ebcc1ad4bb0cf086bbd0476d0
MOON <muneon96@naver.com>
2024-04-20 21:15:23 +0900

    Refactor: 리팩토링

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,11 +1,19 @@
 import styles from "./NavBar.module.scss";
 import classNames from "classnames/bind";
 import { LOGO_IMAGE } from "./constant";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
-export const NavBar = ({ isHiddenButton }) => {
+export const NavBar = ({ isHiddenButton, edit }) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleEdit = (event) => {
+    event.preventDefault();
+    navigate(`/post/${id}/edit`);
+  };
+
   return (
     <nav className={cx("container")}>
       <div className={cx("item")}>
@@ -17,6 +25,13 @@ export const NavBar = ({ isHiddenButton }) => {
             롤링 페이퍼 만들기
           </button>
         </Link>
+        {edit ? (
+          <button className={cx("button")} onClick={handleEdit}>
+            수정하기
+          </button>
+        ) : (
+          ""
+        )}
       </div>
     </nav>
   );
---
+++ src/sharing/TextEditor/TextEditor.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import styles from "./TextEditor.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+/** 텍스트 에디터 */
+export const TextEditor = ({ value, setValue }) => {
+  const modules = {
+    toolbar: [
+      ["bold", "italic", "underline"],
+      [{ align: [] }, { indent: "-1" }, { indent: "+1" }],
+      [{ list: "bullet" }, { list: "ordered" }],
+      [{ color: [] }, { background: [] }],
+    ],
+  };
+
+  const formats = [
+    "bold",
+    "italic",
+    "underline",
+    "align",
+    "indent",
+    "list",
+    "color",
+    "background",
+  ];
+
+  return (
+    <div className={cx("container")}>
+      <h2 className={cx("title")}>내용을 입력해 주세요</h2>
+      <ReactQuill
+        className="editorTest"
+        style={{ height: "340px" }}
+        theme="snow"
+        value={value}
+        modules={modules}
+        formats={formats}
+        onChange={setValue}
+        placeholder="I am your reach text editor."
+      />
+    </div>
+  );
+};
