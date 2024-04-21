28745643f7ffe0a37f92e6444e315ae749623eea
MOON <muneon96@naver.com>
2024-04-21 16:57:50 +0900

    Refactor: EditPage 수정

--- src/pages/EditPage/EditPage.jsx
+++ src/pages/EditPage/EditPage.jsx
@@ -2,114 +2,24 @@ import React, { useState, useEffect } from "react";
 import { Layout } from "layout/Layout";
 import { HeaderService, EmptyPostCard } from "sharing";
 import { useParams } from "react-router-dom";
-import { getRecipientById, getMessages } from "util";
+import { getRecipientById, getMessages, getReactions } from "util";
 import { DELETED_ICON } from "./constant";
-
-const recentMessagesTest = [
-  {
-    id: 10157,
-    recipientId: 5693,
-    sender: "닼닼 모드",
-    profileImageURL: "https://picsum.photos/id/859/100/100",
-    relationship: "동료",
-    content:
-      "<p>다크 모드 너무너무 예쁘네요 :) !!</p><p>모달 바깥 부분 클릭해도 꺼졌으면 좋겠어요!!</p>",
-    font: "나눔명조",
-    createdAt: "2024-03-15T08:28:56.156123Z",
-  },
-  {
-    id: 7743,
-    recipientId: 4814,
-    sender: "코드잇",
-    profileImageURL: "https://picsum.photos/id/571/100/100",
-    relationship: "가족",
-    content: "<p>안녕하세요</p>",
-    font: "나눔명조",
-    createdAt: "2024-03-08T05:29:37.613149Z",
-  },
-  {
-    id: 9872,
-    recipientId: 5626,
-    sender: "테스트에요~",
-    profileImageURL: "https://picsum.photos/id/547/100/100",
-    relationship: "가족",
-    content:
-      '<p><strong>그대들은 어떻게 살것인가 - 미야자키하야오 <span class="ql-cursor">�</span></strong></p>',
-    font: "나눔손글씨 손편지체",
-    createdAt: "2024-03-12T01:27:38.099811Z",
-  },
-];
-
-const topReactionsTest = [
-  {
-    id: 5256,
-    emoji: "😃",
-    count: 2,
-  },
-  {
-    id: 5255,
-    emoji: "😀",
-    count: 2,
-  },
-  {
-    id: 5254,
-    emoji: "🫡",
-    count: 1,
-  },
-];
-
-const reactionsTest = [
-  {
-    id: 5524,
-    emoji: "🥰",
-    count: 3,
-  },
-  {
-    id: 5523,
-    emoji: "😆",
-    count: 2,
-  },
-  {
-    id: 5521,
-    emoji: "😃",
-    count: 1,
-  },
-  {
-    id: 5522,
-    emoji: "😁",
-    count: 1,
-  },
-  {
-    id: 5525,
-    emoji: "😍",
-    count: 1,
-  },
-  {
-    id: 5526,
-    emoji: "😋",
-    count: 1,
-  },
-  {
-    id: 5520,
-    emoji: "😀",
-    count: 1,
-  },
-  {
-    id: 5527,
-    emoji: "🥲",
-    count: 1,
-  },
-];
-
-const nameByIdTest = "hihi";
-const messageCountTest = 23;
 
 export const EditPage = () => {
   const { id } = useParams();
 
+  const setdata = async () => {
+    const recipientData = await getRecipientById(id);
+    const reactions = await getReactions(id, 8);
+    setRecipient(recipientData);
+    setReactions(reactions);
+  };
+
   const [backgroundColor, setBackgroundColor] = useState([]);
   const [backgroundImageURL, setBackgroundImageURL] = useState([]);
   const [recentMessages, setRecentMessages] = useState([]);
+  const [recipient, setRecipient] = useState(null);
+  const [reactions, setReactions] = useState(null);
 
   useEffect(() => {
     const fetchRecipientId = async () => {
@@ -122,7 +32,7 @@ export const EditPage = () => {
       }
     };
     fetchRecipientId();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -137,15 +47,20 @@ export const EditPage = () => {
     fetchMessages();
   }, [id, recentMessages]);
 
+  useEffect(() => {
+    setdata();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
   return (
-    <div>
+    recipient && (
       <Layout isHiddenButton={true}>
         <HeaderService
-          name={nameByIdTest}
-          recentMessages={recentMessagesTest}
-          messageCount={messageCountTest}
-          topReactions={topReactionsTest}
-          reactions={reactionsTest}
+          name={recipient.name}
+          recentMessages={recipient.recentMessages}
+          messageCount={recipient.messageCount}
+          reactions={reactions}
+          id={id}
         />
         <EmptyPostCard
           id={id}
@@ -155,6 +70,6 @@ export const EditPage = () => {
           image={backgroundImageURL}
         />
       </Layout>
-    </div>
+    )
   );
 };
