9f90a5a8843e882da48128a6429cd6477ca43f46
김보민 <bomin@Bomin-MacBook.local>
2024-04-21 23:18:40 +0900

    Feat: list page api 적용

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -3,21 +3,43 @@ import { Link } from "react-router-dom";
 import styles from "./ListPage.module.scss";
 import classNames from "classnames/bind";
 import { CardSlider } from "sharing";
-import { testRecipientsData } from "../../sharing/CardSlider/testdata";
+import { useEffect, useState } from "react";
+import { getRecipients } from "util";
 
 const cx = classNames.bind(styles);
 
 export const ListPage = () => {
+  const [recipientsSortedByReaction, setRecipientsSortedByReaction] = useState(
+    []
+  );
+  const [recipients, setRecipients] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipient = async () => {
+      const recipients = await getRecipients(undefined, undefined, "like");
+      setRecipientsSortedByReaction(recipients);
+    };
+    fetchRecipient();
+  }, []);
+
+  useEffect(() => {
+    const fetchRecipient = async () => {
+      const recipients = await getRecipients();
+      setRecipients(recipients);
+    };
+    fetchRecipient();
+  }, []);
+
   return (
     <Layout isHiddenButton={false}>
       <div className={cx("container")}>
         <div className={cx("rolling-wrapper")}>
           <p className={cx("title")}>인기 롤링 페이퍼 🔥</p>
-          <CardSlider recipients={testRecipientsData} />
+          <CardSlider recipients={recipientsSortedByReaction} />
         </div>
         <div className={cx("rolling-wrapper")}>
           <p className={cx("title")}>최근에 만든 롤링 페이퍼 ⭐</p>
-          <CardSlider recipients={testRecipientsData} />
+          <CardSlider recipients={recipients} />
         </div>
         <Link className={cx("link-button")} to="/post">
           <div className={cx("link-button-style")}>만들어보기</div>
